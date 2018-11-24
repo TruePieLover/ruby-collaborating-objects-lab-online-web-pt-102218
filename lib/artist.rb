c8302f1e783b49a326b75f9cc4753c95bb0ba00d
Brandon Teich <brandonteich123@gmail.com>
2018-11-24 05:39:17 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,3 +7,11 @@ attr_accessor :name, :songs
   def add_song(song)
     self.songs << song   
   end
+  def save
+    @@all << self     #saves all created instances of Artist class in 
+                      #@@all class variable
+  end
+
+  def self.all      #class method all returns all instances of Artist class 
+    @@all
+  end
