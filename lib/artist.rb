36c7ca385e6596aca7a39da1206dac3ba780b1a9
Brandon Teich <brandonteich123@gmail.com>
2018-11-24 05:42:02 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,20 +14,17 @@ class Artist
   def self.all      
     @@all
   end
-
   def self.find_or_create_by_name(name)  
-                                         
     if self.find(name)
       self.find(name)
     else
       self.create(name)
     end
   end
-
   def self.find(name)     
-    self.all.detect { |artist| artist.name == name }
+    self.all.detect do |x| x.name == name 
   end
-
+  end
   def self.create(name)   #class method creates & stores instances vs initializing
     artist = Artist.new(name)
     artist.save
